34edcab7cd4b8d80eb65b6d4c769bce56b154f6c
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-11-30 18:16:47 +0900

    bootup Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElWiki::Application.config.secret_token = '422677b159cb7564aa7c9319da815f0c8d16ac4df2720f8b490aa45ed2011e1c279f5e017e1da5fb736694519c047096d933ebadbe0e706b75a1702cbc88cc7e'
