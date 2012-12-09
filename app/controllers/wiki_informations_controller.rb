ec642a915da3408487fa7fef78b27bf5a915522b
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-09 12:21:38 +0900

    Modified links

--- app/controllers/wiki_informations_controller.rb
+++ app/controllers/wiki_informations_controller.rb
@@ -6,49 +6,49 @@ class WikiInformationsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @wiki_informations }
+      format.json { render json: @wiki_infos }
     end
   end
 
   # GET /wiki_informations/1
   # GET /wiki_informations/1.json
   def show
-    @wiki_information = WikiInformation.find(params[:id])
+    @wiki_info = WikiInformation.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @wiki_information }
+      format.json { render json: @wiki_info }
     end
   end
 
   # GET /wiki_informations/new
   # GET /wiki_informations/new.json
   def new
-    @wiki_information = WikiInformation.new
+    @wiki_info = WikiInformation.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @wiki_information }
+      format.json { render json: @wiki_info }
     end
   end
 
   # GET /wiki_informations/1/edit
   def edit
-    @wiki_information = WikiInformation.find(params[:id])
+    @wiki_info = WikiInformation.find(params[:id])
   end
 
   # POST /wiki_informations
   # POST /wiki_informations.json
   def create
-    @wiki_information = WikiInformation.new(params[:wiki_information])
+    @wiki_info = WikiInformation.new(params[:wiki_information])
 
     respond_to do |format|
-      if @wiki_information.save
-        format.html { redirect_to @wiki_information, notice: 'Wiki information was successfully created.' }
-        format.json { render json: @wiki_information, status: :created, location: @wiki_information }
+      if @wiki_info.save
+        format.html { redirect_to @wiki_info, notice: 'Wiki information was successfully created.' }
+        format.json { render json: @wiki_info, status: :created, location: @wiki_information }
       else
         format.html { render action: "new" }
-        format.json { render json: @wiki_information.errors, status: :unprocessable_entity }
+        format.json { render json: @wiki_info.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -56,15 +56,15 @@ class WikiInformationsController < ApplicationController
   # PUT /wiki_informations/1
   # PUT /wiki_informations/1.json
   def update
-    @wiki_information = WikiInformation.find(params[:id])
+    @wiki_info = WikiInformation.find(params[:id])
 
     respond_to do |format|
-      if @wiki_information.update_attributes(params[:wiki_information])
-        format.html { redirect_to @wiki_information, notice: 'Wiki information was successfully updated.' }
+      if @wiki_info.update_attributes(params[:wiki_information])
+        format.html { redirect_to wiki_information_path(@wiki_info), notice: 'Wiki information was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @wiki_information.errors, status: :unprocessable_entity }
+        format.json { render json: @wiki_info.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -72,8 +72,8 @@ class WikiInformationsController < ApplicationController
   # DELETE /wiki_informations/1
   # DELETE /wiki_informations/1.json
   def destroy
-    @wiki_information = WikiInformation.find(params[:id])
-    @wiki_information.destroy
+    @wiki_info = WikiInformation.find(params[:id])
+    @wiki_info.destroy
 
     respond_to do |format|
       format.html { redirect_to wiki_informations_url }
