52074d609f1a0a40b91b89c5ffba03e2b1c9b855
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-09 23:01:48 +0900

    Clean up association data when delete wiki_information.

--- app/models/wiki_information_observer.rb
+++ app/models/wiki_information_observer.rb
@@ -1,7 +1,13 @@
 class WikiInformationObserver < ActiveRecord::Observer
+  require 'fileutils'
 
   def after_create(wiki_info)
     Grit::Repo.init(WikiInformation::BASE_GIT_DIRECTORY.join("#{wiki_info.name}.git").to_s)
     wiki_info.pages.create(:name => 'Welcome', :body => 'Getting started guide')
   end
+
+  def after_destroy(wiki_info)
+    FileUtils.rm_rf(wiki_info.git_directory)
+  end
+
 end
