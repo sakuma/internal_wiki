228cdab05654f07aac4b78019be65b9f0c508038
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-11 22:46:43 +0900

    in conjunction with wiki name.

--- app/models/wiki_information.rb
+++ app/models/wiki_information.rb
@@ -7,6 +7,8 @@ class WikiInformation < ActiveRecord::Base
   has_many :visible_authority_users, :through => :private_memberships, :source => :user
   belongs_to :creator, :class_name => 'User', :foreign_key => 'created_by'
 
+  before_update :rename_repository_directory
+
   BASE_GIT_DIRECTORY = Rails.root.join('data')
 
   scope :accessible_by, ->(user) do
@@ -14,8 +16,8 @@ class WikiInformation < ActiveRecord::Base
     WikiInformation.where(:is_private => false) + user.private_memberships
   end
 
-  def git_directory
-    BASE_GIT_DIRECTORY.join("#{name}.git").to_s
+  def git_directory(overwrite_name = nil)
+    BASE_GIT_DIRECTORY.join("#{overwrite_name or name}.git").to_s
   end
 
   def collaborator_for_private_wiki?(user)
@@ -23,6 +25,14 @@ class WikiInformation < ActiveRecord::Base
     return false unless membership
     return true if membership.admin?
     false
+  end
+
+  private
+
+  def rename_repository_directory
+    return unless self.name_changed?
+    before, after = self.name_change
+    File.rename(self.git_directory(before), self.git_directory(after))
   end
 
 end
