5f84301acfbcd66dd7b1823b146eaf9414cbe214
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-11 19:19:34 +0900

    Apply I18n for user control pages.

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module InternalWiki
     config.time_zone = 'Tokyo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     # Configure the default encoding used in templates for Ruby 1.9.
