f8d8e440544cbf274d4da84732f9fe04cc028dda
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-19 13:10:57 +0900

    Modified design login page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,17 +7,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:email], params[:password])
+    user = login(params[:email_or_name], params[:password], params[:remember_me])
     if user
-      flash[:notice] = 'Successful logged in.'
+      flash[:notice] = t('terms.successfully_login')
       redirect_back_or_to(root_path)
     else
+      flash.now[:warning] = t('terms.invalid_email_or_name_or_password')
       render :new
     end
   end
 
   def destroy
     logout
-    redirect_to login_path, :notice => 'logout.'
+    redirect_to login_path, :notice => t('terms.logged_out')
   end
 end
