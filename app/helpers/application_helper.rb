a6eae31a511ce37e86e6bb47e9b4324dc538a0b7
Naohiro Sakuma <n.sakuma@everyleaf.com>
2012-12-25 00:12:33 +0900

    Modified user form.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def error_messages_for(obj)
+    if obj.errors.any?
+      content_tag :ul, :class => 'alert alert-error unstyled' do
+        raw "#{obj.errors.full_messages.map{|msg| "<li>#{msg}</li>"}.join('')}"
+      end
+    end
+  end
+
 end
