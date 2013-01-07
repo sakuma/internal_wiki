ed40c65fa0846ec6ffeec8e35229e5dfd5dda249
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-01-07 23:52:48 +0900

    Modified design search word.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def index
-    @search_word = params[:query]
-    result = Groonga::Page.select{|page| page.body =~ @search_word}
-    @pages = Page.where(['id IN(?)', result.map(&:page_id)])
+    @search_word = params[:q]
+    if @search_word.blank?
+      @pages = []
+    else
+      result = Groonga::Page.select{|page| page.body =~ @search_word}
+      @pages = Page.where(['id IN(?)', result.map(&:page_id)])
+    end
   end
 end
