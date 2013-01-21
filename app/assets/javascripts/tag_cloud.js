c04b960a9c04ae0716d094c5eda03caafcad4ee7
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-01-22 03:04:37 +0900

    fix indent

--- app/assets/javascripts/tag_cloud.js
+++ app/assets/javascripts/tag_cloud.js
@@ -22,247 +22,216 @@ var aA=null;
 var oDiv=null;
 
 window.onload=function () {
- var i=0;
- var oTag=null;
+  var i=0;
+  var oTag=null;
 
- oDiv=document.getElementById('tag-cloud');
+  oDiv=document.getElementById('tag-cloud');
 
- aA=oDiv.getElementsByTagName('a');
+  aA=oDiv.getElementsByTagName('a');
 
- for(i=0;i<aA.length;i++)
- {
-  oTag={};
+  for(i=0;i<aA.length;i++) {
+    oTag={};
 
-  oTag.offsetWidth=aA[i].offsetWidth;
-  oTag.offsetHeight=aA[i].offsetHeight;
+    oTag.offsetWidth=aA[i].offsetWidth;
+    oTag.offsetHeight=aA[i].offsetHeight;
 
-  mcList.push(oTag);
- }
+    mcList.push(oTag);
+  }
 
- sineCosine( 0,0,0 );
+  sineCosine( 0,0,0 );
 
- positionAll();
+  positionAll();
 
- oDiv.onmouseover=function ()
- {
-  active=true;
- };
+  oDiv.onmouseover=function () {
+    active=false;
+    // return false
+  };
 
- oDiv.onmouseout=function ()
- {
-  active=false;
- };
+  oDiv.onmouseout=function () {
+    active=true;
+  };
 
- oDiv.onmousemove=function (ev)
- {
-  var oEvent=window.event || ev;
+  oDiv.onmousemove=function (ev) {
+    var oEvent=window.event || ev;
 
-  mouseX=oEvent.clientX-(oDiv.offsetLeft+oDiv.offsetWidth/2);
-  mouseY=oEvent.clientY-(oDiv.offsetTop+oDiv.offsetHeight/2);
+    mouseX=oEvent.clientX-(oDiv.offsetLeft+oDiv.offsetWidth/2);
+    mouseY=oEvent.clientY-(oDiv.offsetTop+oDiv.offsetHeight/2);
 
-  mouseX/=5;
-  mouseY/=5;
- };
+    mouseX/=5;
+    mouseY/=5;
+  };
 
- setInterval(update, 30);
+  setInterval(update, 30);
 
 };
 
-function update()
-{
- var a;
- var b;
+function update() {
+  var a;
+  var b;
 
- if(active)
- {
-  a = (-Math.min( Math.max( -mouseY, -size ), size ) / radius ) * tspeed;
-  b = (Math.min( Math.max( -mouseX, -size ), size ) / radius ) * tspeed;
- }
- else
- {
-  a = lasta * 0.98;
-  b = lastb * 0.98;
- }
+  if(active) {
+    a = (-Math.min( Math.max( -mouseY, -size ), size ) / radius ) * tspeed;
+    b = (Math.min( Math.max( -mouseX, -size ), size ) / radius ) * tspeed;
+  } else {
+    a = lasta * 0.98;
+    b = lastb * 0.98;
+  }
 
- lasta=a;
- lastb=b;
+  lasta=a;
+  lastb=b;
 
- if(Math.abs(a)<=0.01 && Math.abs(b)<=0.01)
- {
-  return;
- }
+  if(Math.abs(a)<=0.01 && Math.abs(b)<=0.01) {
+    return;
+  }
 
- var c=0;
- sineCosine(a,b,c);
- for(var j=0;j<mcList.length;j++)
- {
-  var rx1=mcList[j].cx;
-  var ry1=mcList[j].cy*ca+mcList[j].cz*(-sa);
-  var rz1=mcList[j].cy*sa+mcList[j].cz*ca;
+  var c=0;
+  sineCosine(a,b,c);
+  for(var j=0;j<mcList.length;j++) {
+    var rx1=mcList[j].cx;
+    var ry1=mcList[j].cy*ca+mcList[j].cz*(-sa);
+    var rz1=mcList[j].cy*sa+mcList[j].cz*ca;
 
-  var rx2=rx1*cb+rz1*sb;
-  var ry2=ry1;
-  var rz2=rx1*(-sb)+rz1*cb;
+    var rx2=rx1*cb+rz1*sb;
+    var ry2=ry1;
+    var rz2=rx1*(-sb)+rz1*cb;
 
-  var rx3=rx2*cc+ry2*(-sc);
-  var ry3=rx2*sc+ry2*cc;
-  var rz3=rz2;
+    var rx3=rx2*cc+ry2*(-sc);
+    var ry3=rx2*sc+ry2*cc;
+    var rz3=rz2;
 
-  mcList[j].cx=rx3;
-  mcList[j].cy=ry3;
-  mcList[j].cz=rz3;
+    mcList[j].cx=rx3;
+    mcList[j].cy=ry3;
+    mcList[j].cz=rz3;
 
-  per=d/(d+rz3);
+    per=d/(d+rz3);
 
-  mcList[j].x=(howElliptical*rx3*per)-(howElliptical*2);
-  mcList[j].y=ry3*per;
-  mcList[j].scale=per;
-  mcList[j].alpha=per;
+    mcList[j].x=(howElliptical*rx3*per)-(howElliptical*2);
+    mcList[j].y=ry3*per;
+    mcList[j].scale=per;
+    mcList[j].alpha=per;
 
-  mcList[j].alpha=(mcList[j].alpha-0.6)*(10/6);
- }
+    mcList[j].alpha=(mcList[j].alpha-0.6)*(10/6);
+  }
 
- doPosition();
- depthSort();
+  doPosition();
+  depthSort();
 }
 
-function depthSort()
-{
- var i=0;
- var aTmp=[];
+function depthSort() {
+  var i=0;
+  var aTmp=[];
 
- for(i=0;i<aA.length;i++)
- {
-  aTmp.push(aA[i]);
- }
-
- aTmp.sort
- (
-  function (vItem1, vItem2)
-  {
-   if(vItem1.cz>vItem2.cz)
-   {
-    return -1;
-   }
-   else if(vItem1.cz<vItem2.cz)
-   {
-    return 1;
-   }
-   else
-   {
-    return 0;
-   }
+  for(i=0;i<aA.length;i++) {
+    aTmp.push(aA[i]);
   }
- );
 
- for(i=0;i<aTmp.length;i++)
- {
-  aTmp[i].style.zIndex=i;
- }
+  aTmp.sort
+  (
+    function (vItem1, vItem2) {
+    if(vItem1.cz>vItem2.cz) {
+      return -1;
+    } else if(vItem1.cz<vItem2.cz) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  );
+
+  for(i=0;i<aTmp.length;i++) {
+    aTmp[i].style.zIndex=i;
+  }
 }
 
-function positionAll()
-{
- var phi=0;
- var theta=0;
- var max=mcList.length;
- var i=0;
+function positionAll() {
+  var phi=0;
+  var theta=0;
+  var max=mcList.length;
+  var i=0;
 
- var aTmp=[];
- var oFragment=document.createDocumentFragment();
+  var aTmp=[];
+  var oFragment=document.createDocumentFragment();
 
- //ランダムの順位を付ける
- for(i=0;i<aA.length;i++)
- {
-  aTmp.push(aA[i]);
- }
-
- aTmp.sort
- (
-  function ()
-  {
-   return Math.random()<0.5?1:-1;
+  //ランダムの順位を付ける
+  for(i=0;i<aA.length;i++) {
+    aTmp.push(aA[i]);
   }
- );
 
- for(i=0;i<aTmp.length;i++)
- {
-  oFragment.appendChild(aTmp[i]);
- }
+  aTmp.sort(function () {
+    return Math.random()<0.5?1:-1;
+  });
 
- oDiv.appendChild(oFragment);
-
- for( var i=1; i<max+1; i++){
-  if( distr )
-  {
-   phi = Math.acos(-1+(2*i-1)/max);
-   theta = Math.sqrt(max*Math.PI)*phi;
+  for(i=0;i<aTmp.length;i++) {
+    oFragment.appendChild(aTmp[i]);
   }
-  else
-  {
-   phi = Math.random()*(Math.PI);
-   theta = Math.random()*(2*Math.PI);
-  }
-  //経緯度の変換
-  mcList[i-1].cx = radius * Math.cos(theta)*Math.sin(phi);
-  mcList[i-1].cy = radius * Math.sin(theta)*Math.sin(phi);
-  mcList[i-1].cz = radius * Math.cos(phi);
 
-  aA[i-1].style.left=Math.abs(mcList[i-1].cx+oDiv.offsetWidth/2-mcList[i-1].offsetWidth/2)+'px';
-//  if(mcList[i-1].cx+oDiv.offsetWidth/2>=mcList[i-1].offsetWidth/2){
-//   aA[i-1].style.left=Math.abs(mcList[i-1].cx+oDiv.offsetWidth/2-mcList[i-1].offsetWidth/2)+'px';
-//  }else{
-//   aA[i-1].style.left=Math.abs(mcList[i-1].offsetWidth/2-mcList[i-1].cx+oDiv.offsetWidth/2)+'px';
-//  }
-  aA[i-1].style.top=Math.abs(mcList[i-1].cy+oDiv.offsetHeight/2-mcList[i-1].offsetHeight/2)+'px';
-//  if(mcList[i-1].cy+oDiv.offsetHeight/2>=mcList[i-1].offsetHeight/2){
-//   aA[i-1].style.top=Math.abs(mcList[i-1].cy+oDiv.offsetHeight/2-mcList[i-1].offsetHeight/2)+'px';
-//  }else{
-//   aA[i-1].style.top=Math.abs(mcList[i-1].offsetHeight/2-mcList[i-1].cy-oDiv.offsetHeight/2)+'px';
-//  }
- }
+  oDiv.appendChild(oFragment);
+
+  for( var i=1; i<max+1; i++){
+    if( distr ) {
+      phi = Math.acos(-1+(2*i-1)/max);
+      theta = Math.sqrt(max*Math.PI)*phi;
+    } else {
+      phi = Math.random()*(Math.PI);
+      theta = Math.random()*(2*Math.PI);
+    }
+    //経緯度の変換
+    mcList[i-1].cx = radius * Math.cos(theta)*Math.sin(phi);
+    mcList[i-1].cy = radius * Math.sin(theta)*Math.sin(phi);
+    mcList[i-1].cz = radius * Math.cos(phi);
+
+    aA[i-1].style.left=Math.abs(mcList[i-1].cx+oDiv.offsetWidth/2-mcList[i-1].offsetWidth/2)+'px';
+    //  if(mcList[i-1].cx+oDiv.offsetWidth/2>=mcList[i-1].offsetWidth/2){
+    //   aA[i-1].style.left=Math.abs(mcList[i-1].cx+oDiv.offsetWidth/2-mcList[i-1].offsetWidth/2)+'px';
+    //  }else{
+    //   aA[i-1].style.left=Math.abs(mcList[i-1].offsetWidth/2-mcList[i-1].cx+oDiv.offsetWidth/2)+'px';
+    //  }
+    aA[i-1].style.top=Math.abs(mcList[i-1].cy+oDiv.offsetHeight/2-mcList[i-1].offsetHeight/2)+'px';
+    //  if(mcList[i-1].cy+oDiv.offsetHeight/2>=mcList[i-1].offsetHeight/2){
+    //   aA[i-1].style.top=Math.abs(mcList[i-1].cy+oDiv.offsetHeight/2-mcList[i-1].offsetHeight/2)+'px';
+    //  }else{
+    //   aA[i-1].style.top=Math.abs(mcList[i-1].offsetHeight/2-mcList[i-1].cy-oDiv.offsetHeight/2)+'px';
+    //  }
+  }
 }
 
-function doPosition()
-{
- var l=oDiv.offsetWidth/2;
- var t=oDiv.offsetHeight/2;
- for(var i=0;i<mcList.length;i++)
- {
-  aA[i].style.left=Math.abs(mcList[i].cx+l-mcList[i].offsetWidth/2)+'px';
-//  if(mcList[i].cx+l>=mcList[i].offsetWidth/2){
-//   aA[i].style.left=Math.abs(mcList[i].cx+l-mcList[i].offsetWidth/2)+'px';
-//  }else{
-//   aA[i].style.left=Math.abs(mcList[i].offsetWidth/2-mcList[i].cx-l)+'px';
-//  }
+function doPosition() {
+  var l=oDiv.offsetWidth/2;
+  var t=oDiv.offsetHeight/2;
+  for(var i=0;i<mcList.length;i++) {
+    aA[i].style.left=Math.abs(mcList[i].cx+l-mcList[i].offsetWidth/2)+'px';
+    //  if(mcList[i].cx+l>=mcList[i].offsetWidth/2){
+    //   aA[i].style.left=Math.abs(mcList[i].cx+l-mcList[i].offsetWidth/2)+'px';
+    //  }else{
+    //   aA[i].style.left=Math.abs(mcList[i].offsetWidth/2-mcList[i].cx-l)+'px';
+    //  }
 
-  aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2)+'px';
-//  if(mcList[i].cy+t>=mcList[i].offsetHeight/2+Math.ceil(12*mcList[i].scale/2)+8){
-//   //aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2)+'px';
-//   aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2-Math.ceil(12*mcList[i].scale/2)-8)+'px';
-//  }else{
-//   aA[i].style.top=Math.abs(mcList[i].offsetHeight/2+Math.ceil(12*mcList[i].scale/2)+8-mcList[i].cy-t)+'px';
-//  }
+    aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2)+'px';
+    //  if(mcList[i].cy+t>=mcList[i].offsetHeight/2+Math.ceil(12*mcList[i].scale/2)+8){
+    //   //aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2)+'px';
+    //   aA[i].style.top=Math.abs(mcList[i].cy+t-mcList[i].offsetHeight/2-Math.ceil(12*mcList[i].scale/2)-8)+'px';
+    //  }else{
+    //   aA[i].style.top=Math.abs(mcList[i].offsetHeight/2+Math.ceil(12*mcList[i].scale/2)+8-mcList[i].cy-t)+'px';
+    //  }
 
-  aA[i].style.fontSize=Math.ceil(12*mcList[i].scale/2)+8+'px';
+    aA[i].style.fontSize=Math.ceil(12*mcList[i].scale/2)+8+'px';
 
-  aA[i].style.filter="alpha(opacity="+100*mcList[i].alpha+")";
-  aA[i].style.opacity=mcList[i].alpha;
- }
+    aA[i].style.filter="alpha(opacity="+100*mcList[i].alpha+")";
+    aA[i].style.opacity=mcList[i].alpha;
+  }
 }
 
-function sineCosine( a, b, c)
-{
- sa = Math.sin(a * dtr);
- ca = Math.cos(a * dtr);
- sb = Math.sin(b * dtr);
- cb = Math.cos(b * dtr);
- sc = Math.sin(c * dtr);
- cc = Math.cos(c * dtr);
+function sineCosine( a, b, c) {
+  sa = Math.sin(a * dtr);
+  ca = Math.cos(a * dtr);
+  sb = Math.sin(b * dtr);
+  cb = Math.cos(b * dtr);
+  sc = Math.sin(c * dtr);
+  cc = Math.cos(c * dtr);
 }
 /**
-** author: taobao.jp
-** HP: http://taobao.jp/
-**/
+ ** author: taobao.jp
+ ** HP: http://taobao.jp/
+ **/
 
 
