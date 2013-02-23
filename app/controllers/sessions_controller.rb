73b22af7afa83fbcc44c918c7bf217a406d001c2
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-02-23 17:06:06 +0900

    can be google login only activated user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,17 +24,21 @@ class SessionsController < ApplicationController
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
-      redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
+      redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
     else
       begin
-        @user = add_provider_or_create_by_provider(provider)
-        # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
-        reset_session # protect from session fixation attack
-        auto_login(@user)
-        redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
+        user_hash = Config.send(provider.to_sym).get_user_hash
+        if @user = User.where(email: user_hash[:user_info]["email"]).first
+          @user.authentications.create!(uid: user_hash[:uid], provider: provider)
+          reset_session # protect from session fixation attack
+          auto_login(@user)
+          redirect_to root_path, notice: "Logged in from #{provider.titleize}!"
+        else
+          raise 'oauth failed'
+        end
       rescue => e
         logger.warn "=== #{e.message}"
-        redirect_to login_path, :error => "Failed to login from #{provider.titleize}!"
+        redirect_to login_path, error: "Failed to login from #{provider.titleize}!"
       end
     end
   end
@@ -44,16 +48,4 @@ class SessionsController < ApplicationController
     redirect_to login_path, :notice => t('terms.logged_out')
   end
 
-
-  private
-
-  def add_provider_or_create_by_provider(provider)
-    user_hash = Config.send(provider.to_sym).get_user_hash
-    if exist_user = User.where(email: user_hash[:user_info]["email"]).first
-      exist_user.authentications.create!(uid: user_hash[:uid], provider: provider)
-      exist_user
-    else
-      create_from(provider)
-    end
-  end
 end
