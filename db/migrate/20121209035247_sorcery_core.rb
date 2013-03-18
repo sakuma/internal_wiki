2876f8c7ae6888c707d719530baabfb57c4f256f
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-03-18 17:21:47 +0900

    Refactoring Migration files

--- db/migrate/20121209035247_sorcery_core.rb
+++ db/migrate/20121209035247_sorcery_core.rb
@@ -1,17 +1,13 @@
 class SorceryCore < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :users do |t|
       t.string  :name,             :null => false  # if you use another field as a username, for example email, you can safely remove this field.
-      t.string  :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
-      t.string  :crypted_password, :default => nil
-      t.string  :salt,             :default => nil
+      t.string  :email
+      t.string  :crypted_password
+      t.string  :salt
       t.boolean :admin,            :default => false
 
       t.timestamps
     end
-  end
-
-  def self.down
-    drop_table :users
   end
 end
