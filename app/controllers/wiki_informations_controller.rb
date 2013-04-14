06aab55917740193a3a3cf55311e58837e7cc4b2
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-04-14 22:45:58 +0900

    Using % literal of symbol

--- app/controllers/wiki_informations_controller.rb
+++ app/controllers/wiki_informations_controller.rb
@@ -1,6 +1,6 @@
 class WikiInformationsController < ApplicationController
 
-  before_filter :find_wiki_info, :only => [:show, :edit, :update, :destroy, :add_authority_user, :remove_authority_user]
+  before_filter :find_wiki_info, only: %i[show edit update destroy add_authority_user remove_authority_user]
 
   def index
     @wiki_informations = WikiInformation.accessible_by(current_user)
