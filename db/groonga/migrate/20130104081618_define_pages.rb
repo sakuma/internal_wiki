a680c8838d13c5296cbd212b9d3a5f190f965190
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-04-22 00:43:29 +0900

    Change groonga schema

--- db/groonga/migrate/20130104081618_define_pages.rb
+++ db/groonga/migrate/20130104081618_define_pages.rb
@@ -1,6 +1,6 @@
 class DefinePages < ActiveGroonga::Migration
   def up
-    create_table("pages", :type => :patricia_trie,
+    create_table("pages", :type => :patricia_trie, :normalizer => :NormalizerAuto,
                  :key_type => "ShortText") do |table|
       table.uint64("page_id")
       table.uint64("wiki_information_id")
