2db9e23c737cf11f75c7d83af1faeabbb477055c
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-04-22 00:43:29 +0900

    Change groonga schema

--- db/groonga/migrate/20130105185215_define_full_text_search_index.rb
+++ db/groonga/migrate/20130105185215_define_full_text_search_index.rb
@@ -2,6 +2,7 @@ class DefineFullTextSearchIndex < ActiveGroonga::Migration
   def up
     create_table("bigram",
                  :type => :patricia_trie,
+                 :normalizer => :NormalizerAuto,
                  :key_type => "ShortText",
                  :default_tokenizer => "TokenBigram") do |table|
       table.index("pages.body")
