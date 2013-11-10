0b89c85f3d86037ef1ccacdc7b2460fc07e33c63
Naohiro Sakuma <nao.bear@gmail.com>
2013-11-10 18:12:10 +0900

    change config for production

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails server -p $PORT
+web: bundle exec puma -e $RAILS_ENV -p $PORT
 web-socket: rackup private_pub.ru -s thin -E production
--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 # require "action_controller/railtie"
 # require "action_mailer/railtie"
 # require "active_resource/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  Bundler.require(*Rails.groups(:assets => %w(development test production)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -59,7 +59,7 @@ module InternalWiki
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
