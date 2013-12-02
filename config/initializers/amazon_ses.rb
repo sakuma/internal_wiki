a509e383e08d2a16c7bc1bce2a8dfe28f2defbfa
Naohiro Sakuma <n.sakuma@everyleaf.com>
2013-12-02 19:14:57 +0900

    require SettingLogic class on initializers file.

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,4 +1,5 @@
 if Rails.env.production?
+  require_relative 'settings'
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
     access_key_id: Settings.aws.access_key,
     secret_access_key: Settings.aws.secret_key
